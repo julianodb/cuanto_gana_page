ee784cfed77749012c887ab8da979355412682ff
m3nmql <juliano.barboza@siemens.com>
2021-08-21 11:40:52 -0400

    test list of names and links

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,23 @@
 <template>
+<div class="container">
+  <div class="list is-hoverable">
+    <nuxt-link
+      v-for="name in testNames"
+      :key="name"
+      :to="name"
+      class = "list-item">
+        {{name}}
+    </nuxt-link>
+  </div>
+</div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      testNames: [ "name1", "name2", "name3"]
+    }
+  }
+}
 </script>
