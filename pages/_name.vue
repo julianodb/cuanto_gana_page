25e16a525da8672e07be903a77431e3a063d0c2f
m3nmql <juliano.barboza@siemens.com>
2021-08-22 22:23:15 -0400

    add chart.js & vue-chartjs

--- pages/_name.vue
+++ pages/_name.vue
@@ -2,11 +2,14 @@
   <div class="container">
     <h1 class="title is-1"> {{name}} </h1>
     <div> {{money}} </div>
+    <bar-chart :chartData="paymentDataset" options="" />
   </div>
 </template>
 
 <script>
+import BarChart from '~/components/BarChart.vue'
 export default {
+  components: { BarChart },
   async asyncData({$content,params}) {
     const person = await $content('names')
             .where({ slug: {$eq: params.name}})
@@ -40,6 +43,17 @@ export default {
     return {
       monthsToShow: 10,
       currentMonth: 1
+    }
+  },
+  computed: {
+    paymentDataset: function() {
+      return {
+        labels: this.money.map(m=>m.month),
+        datasets: [{
+          label: 'Remuneración Bruta Mensual',
+          data: this.money.map(m=>m.salaryBeforeTaxes),
+          }]
+      }
     }
   }
 }
