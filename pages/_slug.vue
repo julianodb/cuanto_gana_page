c7802c510a4856b9bd5cc7a5617dc68720b0ad52
m3nmql <juliano.barboza@siemens.com>
2021-08-25 20:42:09 -0400

    add colors to the graphic & remove debug

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -1,13 +1,22 @@
 <template>
   <div class="container">
     <h1 class="title is-1"> {{name}} </h1>
-    <div> {{money}} </div>
     <bar-chart :chartData="paymentDataset" :options="chartOptions" />
   </div>
 </template>
 
 <script>
 import BarChart from '~/components/BarChart.vue'
+
+const graphColors = [
+  'rgba(255, 99, 132, 0.2)',
+  'rgba(255, 159, 64, 0.2)',
+  'rgba(255, 205, 86, 0.2)',
+  'rgba(75, 192, 192, 0.2)',
+  'rgba(54, 162, 235, 0.2)',
+  'rgba(153, 102, 255, 0.2)',
+  'rgba(201, 203, 207, 0.2)'
+]
 export default {
   components: { BarChart },
   async asyncData({$content,params}) {
@@ -66,22 +75,27 @@ export default {
           {
             label: 'Remuneración Bruta Mensual',
             data: this.money.map(m=>m.salaryBeforeTaxes),
+            backgroundColor: graphColors[0]
           },
           {
             label: 'Remuneración Liquida Mensual',
             data: this.money.map(m=>m.salaryAfterTaxes),
+            backgroundColor: graphColors[1]
           },
           {
             label: 'Pago extra diurnas',
             data: this.money.map(m=>m.bonusDayTime),
+            backgroundColor: graphColors[2]
           },
           {
             label: 'Pago extra nocturnas',
             data: this.money.map(m=>m.bonusNightTime),
+            backgroundColor: graphColors[3]
           },
           {
             label: 'Pago extra festivas',
             data: this.money.map(m=>m.bonusHolidays),
+            backgroundColor: graphColors[4]
           }
           ]
       }
